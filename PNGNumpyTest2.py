505c40016212a781a76b97e01244dce6c8372292
rowanfr <beanfun1@gmail.com>
2022-12-08 04:05:20 -0600

    Helper functions have been further refined to simplify input and output of PNG while still keeping processing functions exposed

--- PNGNumpyTest2.py
+++ PNGNumpyTest2.py
@@ -1,32 +1,7 @@
 import numpy
 import png
 import PyPNGHelperFunctions as PNGHelper
-iData = PNGHelper.importPNGtoNumpy('PyPNGTest.png')
-#For some fucking reason one uses filename to indicate physical file's name and file tag to use a seperate library to get something online
-#iData is image data, not the reader
 
-#image data is held as a tuple of (Row, Column, Generator for bytearray of data, PNG metadata dictionary)
-#Generator generates multiple bytearrays that represent each row stacked on top of each other starting at the top and then going dows
-print(iData)
+image_3d = PNGHelper.importPNGto3DNumpy('png.png')
 
-for value in iData:
-    print(value)
-print(iData[3].get('planes'))
-#for value in iData[2]:
-#    print(value)
-#!!!Generators can only generate their variables once so doing this before list yields an empty list
-host = numpy.arange(iData[0]*iData[1]).reshape(iData[1],iData[0])#Reshape works by column then row
-dataMap = list(iData[2])
-print(dataMap)
-image_3d = PNGHelper.ThreeDimensionImageFromBytearray(dataMap, iData[3].get('planes'))#// are integer divisions
-print(image_3d)
-print(image_3d.dtype)
-
-
-f = open('numpyTestOut.png', 'wb')
-w = png.Writer(numpy.size(image_3d,1), numpy.size(image_3d,0), greyscale=False, alpha=True)
-
-print(numpy.copy(image_3d).reshape(image_3d.shape[0],(image_3d.shape[1]*image_3d.shape[2])))
-w.write(f, numpy.copy(image_3d).reshape(image_3d.shape[0],(image_3d.shape[1]*image_3d.shape[2])))
-
-f.close()
+PNGHelper.export3DNumpytoPNG(image_3d, 'numpyTestOut.png')
--- PyPNGHelperFunctions.py
+++ PyPNGHelperFunctions.py
@@ -4,20 +4,57 @@
 import png
 import numpy
 
-def importPNGtoNumpy(filename):
+#This is used to convert the input image into an easier to understand format
+def importPNGto3DNumpy(filename):
     imageData = png.Reader(filename=filename).asDirect()
-    return imageData
+    image_3d = ThreeDimensionImageFromBytearray(imageData[2], imageData[3].get('planes'), imageData[3].get('bitdepth'))
+    return image_3d
 
-def ThreeDimensionImageFromBytearray(bytearrayList, channelQuantity):
+def export3DNumpytoPNG(image_3d, filename):
+    f = open(filename, 'wb')  
+
+    match numpy.size(image_3d,2):
+        case 1:
+            w = png.Writer(numpy.size(image_3d,1), numpy.size(image_3d,0), greyscale=True, alpha=False)
+        case 2:
+            w = png.Writer(numpy.size(image_3d,1), numpy.size(image_3d,0), greyscale=True, alpha=True)
+        case 3:
+            w = png.Writer(numpy.size(image_3d,1), numpy.size(image_3d,0), greyscale=False, alpha=False)
+        case 4:
+            w = png.Writer(numpy.size(image_3d,1), numpy.size(image_3d,0), greyscale=False, alpha=True)
+        case _:
+            raise ValueError("The value provided from the channel dimension is " + numpy.size(image_3d,2) + " which is outside the range of acceptable values")
+    
+    
+    image_2d = semiFlatten3DNumpyArray(image_3d)
+    w.write(f, image_2d)
+
+    f.close()
+
+#This is used to convert the numpy array to it's final output for PyPNG
+def semiFlatten3DNumpyArray(numpyArray):
+    image_2d = numpyArray.reshape(numpy.size(numpyArray,0),(numpy.size(numpyArray,1)*numpy.size(numpyArray,2)))
+    return image_2d
+
+def ThreeDimensionImageFromBytearray(bytearrayList, channelQuantity, bitdepth):
     if channelQuantity < 1 or channelQuantity > 4:
         raise ValueError("PyPNG only offers L (Greyscale), LA (Greyscale with alpha), RGB (Red, Green, and Blue), and RGBA channels. The function ThreeDimensionImageFromBytearray was given the input of " + str(channelQuantity) + " channels to use which falls outside of the range of channels that PyPNG can offer.")
-    image_2d = TwoDimensionImageFromBytearray(bytearrayList)
+    image_2d = TwoDimensionImageFromBytearray(bytearrayList, bitdepth)
     image_3d = image_2d.reshape(numpy.size(image_2d,0),(numpy.size(image_2d,1)//channelQuantity),channelQuantity)
     return image_3d
 
-
-def TwoDimensionImageFromBytearray(bytearrayList):
-    image_2d = numpy.vstack(bytearrayList)
+#bitdepth here is per channel, not per pixel, hence it only include 2^x, x from 0 to 4
+def TwoDimensionImageFromBytearray(bytearrayList, bitdepth):
+    firstPass = True
+    datatypeDictionary = {1 : numpy.bool, 8 : numpy.uint8, 16 :numpy.uint16}
+    #Bit depths of 2 and 4 can be supported later
+    for row in bytearrayList:
+        if firstPass:
+            image_2d = numpy.frombuffer(row, dtype = datatypeDictionary.get(bitdepth))
+            firstPass = False
+        else:
+            byteRow = numpy.frombuffer(row, dtype = datatypeDictionary.get(bitdepth))
+            image_2d = numpy.vstack((image_2d, byteRow))
     return image_2d
 
 def OutputImage(filename,):
